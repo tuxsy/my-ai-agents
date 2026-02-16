5259e8de84d76bb28ae0987e198abfb3eb41e055
Bru Anglés Robles <bangles@gmail.com>
2026-02-16 19:55:14 +0100

    feat: call the availability tool i it is needed

--- simple-agent/agent.py
+++ simple-agent/agent.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from groq import Groq
 import os
 from simple_memory import SimpleMemory
+import json
+from tools import Tools
 
 load_dotenv()
 
@@ -11,9 +13,110 @@ api_key = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=api_key)
 memory = SimpleMemory(max_messages=MEMORY_MAX_MESSAGES)
+agent_tools = Tools(credentials_path=os.getenv("GOOGLE_CREDENTIALS_PATH"))
+TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_current_datetime",
+            "description": (
+                "Obtiene la fecha y hora actual en formato RFC3339."
+                "Útil para que el asistente pueda conocer la fecha y hora actual y así responder preguntas relacionadas con el tiempo"
+                "o programar eventos."
+            ),
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "check_availability",
+            "description": (
+                "Revisa si el calendario del usuario está disponible entre time_start y time_end."
+                "Los datos time_start y time_end deben estar en el formato RFC3339 (sin incluir offset para la zona horaria),"
+                "por ejemplo: 2024-01-01T10:00,"
+            ),
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "time_start": {
+                        "type": "string",
+                        "description": "La fecha de inicio para verificar la disponibilidad, en formato RFC3339 sin offset para la zona horaria."
+                    },
+                    "time_end": {
+                        "type": "string",
+                        "description": "La fecha de fin para verificar la disponibilidad, en formato RFC3339 sin offset para la zona horaria."
+                    }
+                },
+                "required": [
+                    "time_start",
+                    "time_end"
+                ]
+            }
+        }
+    },
+]
 
-print("Agente de IA maleducado")
+print("Agente de IA iniciado. Escribe 'salir' para terminar la conversación.")
 
+
+def process_response(client: Groq, tools: Tools, messages: list[dict], user_input: str):
+    # Agregamos el mensaje del usuario a la conversación
+    messages.append(
+        {"role": "user", "content": user_input},
+    )
+
+    while True:
+        resp = client.chat.completions.create(
+            model="qwen/qwen3-32b",
+            messages=messages,
+            tools=TOOLS,
+        )
+
+        msg = resp.choices[0].message
+
+        # Si es un simple texto, lo mostramos y terminamos
+        if not getattr(msg, "tool_calls", None):
+            return msg.content or ""
+
+        messages.append({
+            "role": "assistant",
+            "content": msg.content or "",
+            "tool_calls": [tc.model_dump() for tc in msg.tool_calls]
+        })
+
+        # Si es una llamada a una herramienta:
+        #  1. Ejecutamos la herramienta
+        #  2. Agregamos el resultado a la conversación
+        #  3. Volvemos a llamar al modelo para que genere una respuesta final
+        #  4. Si es un texto lo mostramos, si es otra heramienta, repetimos el proceso.
+
+        for tool_call in msg.tool_calls:
+            name = tool_call.function.name
+            args = json.loads(tool_call.function.arguments or {})
+
+            print(
+                f"Llamada a herramienta {name} con argumentos {args}")
+
+            if name == "get_current_datetime":
+                tool_response = tools.get_current_datetime()
+            elif name == "check_availability":
+                tool_response = tools.check_availability(
+                    time_start=args.get("time_start"),
+                    time_end=args.get("time_end"),
+                )
+            else:
+                print(f"Herramienta {name} no encontrada")
+                tool_response = {"error": f"Herramienta {name} no encontrada"}
+
+            # Agregar resultado de la herramienta a la conversación
+            messages.append({
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "content": json.dumps(tool_response, ensure_ascii=False),
+            })
+
+
+# Bucle principal de interacción con el usuario
 while True:
 
     user_text = input("Tú: ").strip()
@@ -25,23 +128,10 @@ while True:
         print("Adiós.")
         break
 
-    # Leer memoria
-    messages = memory.messages()
-    messages.append(
-        {"role": "user", "content": user_text},
-    )
-
-    resp = client.chat.completions.create(
-        model="qwen/qwen3-32b",
-        messages=messages,
-    )
-
-    msg = resp.choices[0].message
-    assistan_text = msg.content or ""
-
+    assistan_text = process_response(
+        client, agent_tools, memory.messages(), user_text)
     print(f"Asistente: {assistan_text}")
 
     # Guardar en memoria
     memory.add(role="user", content=user_text)
     memory.add(role="assistant", content=assistan_text)
-
--- simple-agent/tools.py
+++ simple-agent/tools.py
@@ -3,31 +3,42 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 
 class Tools:
 
     def __init__(self, credentials_path: str, temp_dir: str = ".temp"):
-        self.SCOPES = ["https://www.googleapis.com/auth/calendar"]
         self.credentials_path = credentials_path
+        if not os.path.exists(self.credentials_path):
+            raise FileNotFoundError(
+                "Archivo de credenciales no encontrado")
+
+        self.SCOPES = ["https://www.googleapis.com/auth/calendar"]
         self.token_path = os.path.join(temp_dir, "token.json")
         if not os.path.exists(temp_dir):
             os.makedirs(temp_dir)
+
+    def get_current_datetime(self):
+        return {
+            "current_datetime": datetime.now().isoformat(),
+        }
 
     def get_calendar_services(self):
         creds = None
         # Si el token existe lo cargamos
         if os.path.exists(self.token_path):
-            creds = Credentials.from_authorized_user_file(self.token_path, self.SCOPES)
+            creds = Credentials.from_authorized_user_file(
+                self.token_path, self.SCOPES)
 
         # Refrescar token o solicitar uno nuevo
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                if not os.path.exists(self.credentials_path):
-                    raise FileNotFoundError("Archivo de credenciales no encontrado")
-                
-                flow = InstalledAppFlow.from_client_secrets_file(self.credentials_path, self.SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.credentials_path, self.SCOPES)
                 creds = flow.run_local_server(port=0)
                 with open(self.token_path, "w") as token:
                     token.write(creds.to_json())
@@ -35,8 +46,10 @@ class Tools:
         # Generar servicio de Google Calendar
         return build("calendar", "v3", credentials=creds)
 
-    def check_availability(self, time_start: str, time_end: str, calendar: str = "primary"):
-        print(f"Llamada a herramientea check_availability con time_start={time_start} y time_end={time_end}")
+    def check_availability(self, time_start: str, time_end: str, calendar: str = "primary", timezone: str = "Europe/Madrid"):
+        time_start = add_zoneinfo(time_start, timezone)
+        time_end = add_zoneinfo(time_end, timezone)
+        
         body = {
             "timeMin": time_start,
             "timeMax": time_end,
@@ -44,7 +57,8 @@ class Tools:
         }
         service = self.get_calendar_services()
         events_result = service.freebusy().query(body=body).execute()
-        busy_times = events_result.get("calendars", {}).get(calendar, {}).get("busy", [])
+        busy_times = events_result.get("calendars", {}).get(
+            calendar, {}).get("busy", [])
         return {
             "calendar_id": calendar,
             "time_start": time_start,
@@ -54,10 +68,19 @@ class Tools:
         }
 
 
+
+# Utility functions
+
+def add_zoneinfo(dt_str: str, timezone: str):
+    dt = datetime.fromisoformat(dt_str)
+    dt = dt.replace(tzinfo=ZoneInfo(timezone))
+    return dt.isoformat(timespec="seconds")
+
 if __name__ == "__main__":
     from dotenv import load_dotenv
     load_dotenv()
 
     tools = Tools(credentials_path=os.getenv("GOOGLE_CREDENTIALS_PATH"))
-    result = tools.check_availability("2026-02-16T17:00:00+01:00", "2026-02-16T17:30:00+01:00")
+    result = tools.check_availability(
+        "2026-02-16T17:00:00+01:00", "2026-02-16T17:30:00+01:00")
     print(result)
